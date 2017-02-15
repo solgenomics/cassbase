a7fa8b62699bfa908218109ae49b87bfab1897bc
nickmorales <nmorales3142@gmail.com>
2017-02-15 13:41:04 -0500

    add cvterms script

--- bin/save_list_of_cvterms.pl
+++ bin/save_list_of_cvterms.pl
@@ -1,18 +1,19 @@
 #!/usr/bin/env perl
 
 =head1
-save_list_of_terms.pl
+save_list_of_cvterms.pl
 
 =head1 SYNOPSIS
 
-    perl save_list_of_terms.pl -H localhost -D fixture2 -i input_list.txt -c stringtie_transcripts
+    perl save_list_of_cvterms.pl -H localhost -D fixture2 -i input_list.txt -d CASSFT -c postcomposed_terms
 
 =head1 COMMAND-LINE OPTIONS
 
  -H  host name
  -D  database name
  -i  input list file. should be a flat list of traits, each on their own line. E.g. Manes.05G094400.1.v6.1|ST:0302847
- -c  cv name to save these terms under
+ -d  db name to save these terms under
+ -c cv name to save these terms under
 
 =head2 DESCRIPTION
 
@@ -39,13 +40,13 @@ use Data::Dumper;
 use SGN::Model::Cvterm;
 use Try::Tiny;
 
-our ($opt_H, $opt_D, $opt_i, $opt_c);
-getopts('H:D:i:c:');
+our ($opt_H, $opt_D, $opt_i, $opt_d, $opt_c);
+getopts('H:D:i:d:c:');
 
 
 
-if (!$opt_D || !$opt_H || !$opt_i || !$opt_c) {
-  die("Exiting: options missing\nRequires: -D -H -i -c");
+if (!$opt_D || !$opt_H || !$opt_i || !$opt_d || !$opt_c) {
+  die("Exiting: options missing\nRequires: -D -H -i -d -c");
 }
 
 my $dbh = CXGN::DB::InsertDBH
@@ -61,18 +62,23 @@ my $schema = Bio::Chado::Schema->connect(  sub { $dbh->get_actual_dbh() } );
 my $cv = $schema->resultset("Cv::Cv")->find_or_create({name=>$opt_c});
 my $cv_id = $cv->cv_id();
 
+my $db = $schema->resultset("General::Db")->find_or_create({name=>$opt_d});
+my $db_id = $db->db_id();
+
+my $q = "SELECT accession FROM dbxref WHERE db_id=$db_id ORDER BY dbxref_id DESC LIMIT 1;";
+my $h = $dbh->prepare($q);
+$h->execute();
+my ($last_accession) = $h->fetchrow_array();
+my $accession = $last_accession + 1;
+
 open(my $fh, '<', $opt_i)
     or die "Could not open file '$opt_i' $!";
 
 while (my $row = <$fh>) {
-    my ($term, $db_acc) = split '\|', $row;
-    my ($db, $acc) = split '\:', $db_acc;
-
-    my $db = $schema->resultset("General::Db")->find_or_create({name=>$db});
-    my $db_id = $db->db_id();
-    my $dbxref = $schema->resultset("General::Dbxref")->create({db_id=>$db_id, accession=>$acc});
+    my $dbxref = $schema->resultset("General::Dbxref")->create({db_id=>$db_id, accession=>$accession});
     my $dbxref_id = $dbxref->dbxref_id();
-    my $cvterm = $schema->resultset("Cv::Cvterm")->create({cv_id=>$cv_id, name=>$term, dbxref_id=>$dbxref_id});
+    my $cvterm = $schema->resultset("Cv::Cvterm")->create({cv_id=>$cv_id, name=>$row, dbxref_id=>$dbxref_id});
+    $accession++;
 }
 
 
